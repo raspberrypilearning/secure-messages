402d965d80cf743ea1af9d95045675710cf860d1
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-08-09 15:29:16 +0100

    New translations friendship-calculator-finished-friends.py (Polish)

--- pl-PL/resources/friendship-calculator-finished-friends.py
+++ pl-PL/resources/friendship-calculator-finished-friends.py
@@ -1,15 +1,15 @@
 #!/bin/python3
 
 wynik = 0
-imiona = input('wprowadz imiona 2 ludzi: ')
+imiona = input('wprowadź imiona 2 ludzi: ')
 
-for litera in wiadomość:
+for litera in imiona:
   if litera in 'aeiou':
-    wynik + = 5
+    wynik += 5
   if litera in 'przyjaciel':
-    wynik + = 10
+    wynik += 10
 
-print('wynik twojej przyjazni to :', wynik)
+print('wynik twojej przyjaźni to :', wynik)
 
 if wynik > 100:
   print ('najlepsi przyjaciele!')
