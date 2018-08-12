9f26c67f27fa0e4dbbbf293a93e019b63fdfe702
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-08-12 14:51:07 +0100

    New translations friendship-calculator-finished-friends.py (Italian)

--- it-IT/resources/friendship-calculator-finished-friends.py
+++ it-IT/resources/friendship-calculator-finished-friends.py
@@ -1,15 +1,15 @@
 #!/bin/python3
 
-score = 0
-names = input('enter tha names of 2 people: ')
+punteggio = 0
+nomi = input('Scrivi i nomi di 2 persone: ')
 
-for character in names:
-  if character in 'aeiou':
-    score += 5
-  if character in 'friend':
-    score += 10
+for lettera in nomi:
+  if lettera in 'aeiou':
+    punteggio += 5
+  if lettera in 'amici':
+    punteggio += 10
 
-print('your friendship score is :', score)
+print('il vostro livello di amicizia è :', punteggio)
 
 if score > 100:
   print('best friends!')
