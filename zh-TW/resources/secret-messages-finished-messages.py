e1aece533b969dec41c7f9ed8677ffee3d9dbdb6
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-06-26 20:42:29 +0100

    New translations secret-messages-finished-messages.py (Chinese Traditional)

--- zh-TW/resources/secret-messages-finished-messages.py
+++ zh-TW/resources/secret-messages-finished-messages.py
@@ -3,9 +3,9 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 newMessage = ''
   
-message = input('Please enter a message: ')
+message = input('請輸入一個訊息: ')
 
-key = input('Enter a key (1-26): ')
+key = input('輸入一個鑰匙 (1-26): ')
 key = int(key)
 
 for character in message:
@@ -17,4 +17,4 @@ for character in message:
   else:
     newMessage += character
 
-print('Your new message is: ', newMessage)
+print('你的新訊息是: ', newMessage)
